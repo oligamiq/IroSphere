683a6d747aeca11a03e568526c9fb61372f8ac9f
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2023-04-05 15:50:03 +0900

    move color sphere

--- src/3dviewer.ts
+++ src/3dviewer.ts
@@ -1,5 +1,10 @@
 import { appWindow } from '@tauri-apps/api/window';
+import { TGALoader } from 'three/examples/jsm/loaders/TGALoader';
+import GridHorizontal from './assets/GridHorizontal.tga?url'
+import GridVertical from './assets/GridVertical.tga?url'
 import * as THREE from 'three';
+import { register, unregister } from '@tauri-apps/api/globalShortcut';
+import { event } from '@tauri-apps/api';
 
 export function threeviewer(threecanvas: any, param: { initNodeNumS: number, initNodeNumL: number, initNodeNumH: number }) {
     let unlisten_resize: any;
@@ -40,25 +45,44 @@ export function threeviewer(threecanvas: any, param: { initNodeNumS: number, ini
         group.add(mesh)
     }
 
-    var parentID = 0;
-
     for (var k = 0; k < param.initNodeNumL; k++) {
         const elevation = param.initNodeNumL == 1 ? 0.0 : Math.PI * 2 / param.initNodeNumL * k;
-
         for (var j = 1; j <= param.initNodeNumS; j++) {
-
             const radius = 1 / param.initNodeNumS * j;
-
             for (var i = 0; i < param.initNodeNumH; i++) {
                 const azimuth = param.initNodeNumH == 1 ? 0.0 : Math.PI / (param.initNodeNumH - 1) * i + Math.PI / 2;
                 const rot = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), azimuth).multiply(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), elevation));
                 const pos = new THREE.Vector3(0, 0, radius).applyQuaternion(rot);
-                parentID = parentID == 0 ? 1 : 0;
                 createMesh(0.01, pos, [10, 5], j == param.initNodeNumS)
             }
         }
     }
 
+    // 軸の表示
+    const axisGroup = new THREE.Group();
+    var axisMeshVertical = new THREE.Group();
+    var axisMeshHorizontal = new THREE.Group();
+    new TGALoader().load(GridVertical, texture => {
+        const axis_geometry = new THREE.PlaneGeometry(2, 2, 10, 10);
+        const axis_material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ map: texture, transparent: true, side: THREE.DoubleSide });
+        axis_material.alphaTest = 0.01
+        const axis_mesh = new THREE.Mesh(axis_geometry, axis_material);
+        const axis_mesh_cross = new THREE.Mesh(axis_geometry, axis_material);
+        axis_mesh_cross.rotation.y += Math.PI / 2;
+        axisMeshVertical.add(axis_mesh)
+        axisMeshVertical.add(axis_mesh_cross)
+    })
+    new TGALoader().load(GridHorizontal, texture => {
+        const axis_geometry = new THREE.PlaneGeometry(2, 2, 10, 10);
+        const axis_material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ map: texture, transparent: true, side: THREE.DoubleSide });
+        axis_material.alphaTest = 0.01
+        const axis_mesh = new THREE.Mesh(axis_geometry, axis_material);
+        axis_mesh.rotation.x += Math.PI / 2;
+        axisMeshHorizontal.add(axis_mesh)
+    })
+    axisGroup.add(axisMeshVertical)
+    axisGroup.add(axisMeshHorizontal)
+    group.add(axisGroup)
 
     async function f() {
         unlisten_resize = await appWindow.onResized(({ payload: size }) => {
@@ -68,14 +92,40 @@ export function threeviewer(threecanvas: any, param: { initNodeNumS: number, ini
             camera.right = aspect_camera[0]
             camera.top = aspect_camera[1]
             camera.bottom = -aspect_camera[1]
-            // camera.aspect = (size.width / 2) / size.height
             camera.updateProjectionMatrix();
         });
 
+        var x_speed: number = 0;
+        var y_speed: number = 0;
+        let key_down: { w: boolean, a: boolean, s: boolean, d: boolean };
+        key_down = { w: false, a: false, s: false, d: false }
+        const key_func = function (event: KeyboardEvent, press: boolean) {
+            if (event.key == "w")
+                key_down.w = press
+            if (event.key == "a")
+                key_down.a = press
+            if (event.key == "s")
+                key_down.s = press
+            if (event.key == "d")
+                key_down.d = press
+        }
+        document.addEventListener('keydown', e => key_func(e, true), false); // 第一引数にkeydownを記述
+        document.addEventListener('keyup', e => key_func(e, false), false); // 第一引数にkeydownを記述
+
         function animate() {
             for (var i = 0; i < 1; i++) {
-                // group.rotation.x += 0.01;
-                group.rotation.y += 0.01;
+                if (key_down.w)
+                    x_speed += 0.005
+                if (key_down.a)
+                    y_speed += 0.005
+                if (key_down.s)
+                    x_speed -= 0.005
+                if (key_down.d)
+                    y_speed -= 0.005
+                group.rotation.x += x_speed;
+                x_speed *= 0.85;
+                group.rotation.y += y_speed;
+                y_speed *= 0.85;
             }
             requestAnimationFrame(animate);
             renderer.render(scene, camera);
--- src/hotKey.ts
+++ src/hotKey.ts
@@ -1,12 +1,24 @@
 import { invoke } from '@tauri-apps/api';
-import { register } from '@tauri-apps/api/globalShortcut';
+import { register, unregister } from '@tauri-apps/api/globalShortcut';
 import { appWindow } from '@tauri-apps/api/window';
 
 // https://tauri.app/v1/api/js/globalshortcut/#docusaurus_skipToContent_fallback
 export function registerShortcut() {
     // https://tauri.app/v1/api/js/window/#setfullscreen
-    register('F11', () => appWindow.isFullscreen().then(isFullScreen => appWindow.setFullscreen(!isFullScreen)))
-    register('Ctrl+O', () => {
-        invoke('open_file_dialog', {})
-    })
+    // this is global hotkey
+    // unregister('F11').then(() => register('F11', () => appWindow.isFullscreen().then(isFullScreen => appWindow.setFullscreen(!isFullScreen))))
+    // unregister('Ctrl+O').then(() => register('Ctrl+O', () => {
+    //     invoke('open_file_dialog', {})
+    // }))
+
+    document.addEventListener('keyup', e => {
+        if (e.key == 'F11')
+            appWindow.isFullscreen().then(isFullScreen => appWindow.setFullscreen(!isFullScreen))
+        if (e.ctrlKey)
+            if (e.key == 'o')
+                invoke('open_file_dialog', {})
+        if (e.key == 'Escape')
+            appWindow.setFullscreen(false)
+    }, false); // 第一引数にkeydownを記述
+
 }
