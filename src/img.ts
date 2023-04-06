25db485fc54b87cb4f3fe9e7240e3e725c262e01
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2023-04-06 20:42:52 +0900

    test

--- src/img.ts
+++ src/img.ts
@@ -1,3 +1,4 @@
+import { invoke } from '@tauri-apps/api';
 import { convertFileSrc } from '@tauri-apps/api/tauri'
 import { listen } from '@tauri-apps/api/event'
 import { appWindow } from '@tauri-apps/api/window'
@@ -16,16 +17,18 @@ export function img_load_init() {
     let unlisten: any;
     let unlisten_resize: any;
 
+    const load_img = (size: [number, number]) => {
+        img_src_size = size
+        const aspect = img_size_aspect(size, [window.innerWidth / 2, window.innerHeight])
+        file_img!.width = aspect[0]
+        file_img!.height = aspect[1]
+    }
+
     const file_img = document.querySelector<HTMLImageElement>("#file_img");
     file_img!.crossOrigin = 'anonymous'
     if (file_img?.parentElement) {
         file_img.src = Array(ColorBarSample, FlowerSample, KawaiiSample, MountainSample, PenguinSample, WindmillSample)[randInt(0, 5)]
-        get_img_src_size(file_img.src, size => {
-            img_src_size = size
-            const aspect = img_size_aspect(size, [window.innerWidth / 2, window.innerHeight])
-            file_img!.width = aspect[0]
-            file_img!.height = aspect[1]
-        })
+        get_img_src_size(file_img.src, load_img)
     }
 
     // https://qiita.com/mrin/items/efe899943c3f69d53353
@@ -36,14 +39,36 @@ export function img_load_init() {
             console.log('User dropped', ev.payload.paths);
             const [filepath] = ev.payload.paths// as string[]
             img_load(convertFileSrc(filepath))
-            get_img_src_size(convertFileSrc(filepath), size => {
-                img_src_size = size
-                const aspect = img_size_aspect(size, [window.innerWidth / 2, window.innerHeight])
-                file_img!.width = aspect[0]
-                file_img!.height = aspect[1]
-            })
+            get_img_src_size(convertFileSrc(filepath), load_img)
         }
     })
+
+    document.addEventListener('keyup', e => {
+        if (e.ctrlKey && e.key == 'o') {
+            try {
+                invoke('open_file_dialog', {})
+            }
+            catch {
+                const input = document.createElement('input');
+                input.type = 'file';
+                input.accept = 'image/*';
+                input.onchange = event => {
+                    const file = (event!.target! as HTMLInputElement)!.files![0];
+                    const reader = new FileReader();
+                    reader.readAsDataURL(file);
+                    reader.onload = (e) => {
+                        const filepath_impl = e.target!.result!;
+                        if (typeof (filepath_impl) == 'string') {
+                            const filepath = filepath_impl
+                            img_load(filepath)
+                            get_img_src_size(filepath, load_img)
+                        }
+                    };
+                };
+                input.click();
+            }
+        }
+    }, false);
 
     async function f() {
         unlisten = await listen('img_load', event => {
